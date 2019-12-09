5817c9e82691f67dd6541868c9adb16b0d782903
alex-Symbroson <alex.Symbroson@gmail.com>
2019-12-09 21:22:25 +0100

    added 9

--- 2019/aoc-07-2.lisp
+++ 2019/aoc-07-2.lisp
@@ -86,22 +86,9 @@
             (when (> cmd 8) (print (list "unknown command" cmd)) (exit -1))
 ) out))
 
-;;; initializing
-
-;(setq inp '(3 52 1001 52 -5 52 3 53 1 52 56 54 1007 54 5 55 1005 55 26 1001 54
-;-5 54 1105 1 12 1 53 54 53 1008 54 0 55 1001 55 1 55 2 53 55 53 4
-;53 1001 56 -1 56 1005 56 6 99 0 0 0 0 10))
-
-;(setq inp '(3 26 1001 26 -4 26 3 27 1002 27 2 27 1 27 26
-;27 4 27 1001 28 -1 28 1005 28 6 99 0 0 5))
-
-(setq amps nil)
-
-(setq u 5 v 9 phases nil)
-
 ;;; execution
-
-;>10355224
+(setq amps nil)
+(setq u 5 v 9 phases nil)
 
 (print
 (loop for a from u to v maximize
