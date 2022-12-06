e5ab32988db63bd94f33b9e91f3cb4e8b0d1e4dc
alex-Symbroson <alex.symbroson@gmail.com>
2022-12-06 06:13:32 +0100

    22-06

---
+++ 2022/06.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+let input: String = readinput(n: "input")
+//"mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+
+let s = input
+
+for i in 0..<input.count-4 {
+    let t = s.prefix(i+14).suffix(14)
+    if Set(t).count == 14 { print(i+14); break }
+}
