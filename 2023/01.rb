6e1e7b99457b0a480d4ad067b18836817c8865b3
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-01 12:27:54 +0100

    minor change

--- 2023/01.rb
+++ 2023/01.rb
@@ -5,6 +5,9 @@ print('Part 1: ', digits.sum { |num| (num.first + num.last).to_i }, "\n")
 
 names = %w[_ one two three four five six seven eight nine]
 digits = input.map do |line|
-    line.gsub(Regexp.new("(?=(#{names.join('|')}))")) { |_| names.index(Regexp.last_match(1)).to_s }.scan(/\d/)
+    line.gsub(Regexp.new("(?=(#{names.join('|')}))")) do |_|
+        names.index($1).to_s
+    end.scan(/\d/)
 end
+
 print('Part 2: ', digits.sum { |num| (num.first + num.last).to_i })
