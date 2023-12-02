ab9b189ef9d4283304f9443f9401b5bc7a22c1c3
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-02 10:41:15 +0100

    optimized 23-02

--- 2023/02.rb
+++ 2023/02.rb
@@ -1,4 +1,5 @@
 input = File.readlines('input.txt')
+max = { 'r' => 12, 'g' => 13, 'b' => 14 }
 
 def sumTurns(turns, &op)
     turns.reduce({}) do |sum, turn|
@@ -8,16 +9,12 @@ end
 
 games = input.map do |line|
     line.split(': ')[1].split('; ').map do |turn|
-        sumTurns(
-            turn.split(', ').map do |cubes|
-                t = cubes.split
-                [[t[1][0], t[0].to_i]].to_h
-            end
-        ) { |a, b| a + b }
+        turn.split(', ').to_h do |cubes|
+            t = cubes.split
+            [t[1][0], t[0].to_i]
+        end
     end
 end
-
-max = { 'r' => 12, 'g' => 13, 'b' => 14 }
 
 print('Part 1: ', games.map.with_index do |turn, i|
     turn.any? { |t| t.any? { |k, v| v > max[k] } } ? 0 : i + 1
