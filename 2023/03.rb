dd301132537a0ab527642198b6bfc053c839727b
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-03 15:48:53 +0100

    optimized 23-03

--- 2023/03.rb
+++ 2023/03.rb
@@ -1,34 +1,21 @@
 input = File.readlines('input.txt')
-res = []
-map = {}
-sum = 0
+input = ['', *input]
+parts = []
+gears = {}
 
-for i in 0...input.length
-    line = input[i]
-    offset = 0
-    while m = line.match(/\d+/, offset)
-        offset = m.end(0) + 1
-        num = $&.to_i
-        range = [m.begin(0) - 1, 0].max...(m.end(0) + 1)
-        part = nil
-        part = [$&, $~] if i > 0 && input[i - 1][range].match(/[^\d\n.]/)
-        part = [$&, $~] if input[i][range].match(/[^\d\n.]/)
-        part = [$&, $~] if i < input.length - 1 && input[i + 1][range].match(/[^\d\n.]/)
+for i in 1...input.length
+    input[i].gsub(/\d+/) do |m|
+        range = ($~.begin(0) - 1).clamp(0..)...$~.end(0) + 1
+        part = input[(i - 1).clamp(0..)...i + 2].map.with_index do |ln, y|
+            [ln[range]&.match(/[^\d\n.]/), y + i - 1]
+        end.find { |x| x[0] }
         next unless part
 
-        res.push(num)
-
-        # part 2
-        m = part[1]
-        key = "#{i - 1},#{m.begin(0) + range.begin}"
-        if part[0] == '*' && map[key] && map[key] != -1
-            sum += map[key] * num
-            map[key] = -1
-        else
-            map[key] = num
-        end
+        parts.push(m.to_i)
+        key = "#{part[1]},#{part[0].begin(0) + range.begin}"
+        gears[key] = gears.fetch(key, 1) * -m.to_i if part[0].match(0) == '*'
     end
 end
 
-print('Part 1: ', res.sum, "\n")
-print('Part 2: ', sum)
+print('Part 1: ', parts.sum, "\n")
+print('Part 2: ', gears.values.filter(&:positive?).sum)
