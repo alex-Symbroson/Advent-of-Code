fa760891a602744ba4f758e7a7d68d448869f812
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-04 12:05:26 +0100

    23-04 golf update

--- 2023/04 golf.rb
+++ 2023/04 golf.rb
@@ -1,5 +1,4 @@
-w=File.readlines('input.txt').map{|l|l.split(/:|\|/)[1,2].
-map{_1.split.map(&:to_i)}.reduce(&:&).size}
-puts"Part1:#{(w-[0]).map{2.pow(_1-1)}.sum}"
+w=File.readlines('input.txt').map{_,b,c=_1.split(/:|\|/);(b.split&c.split).size}
+puts"Part1:#{(w-[0]).sum{2**(_1-1)}}"
 c=w.map{1};w.each.with_index{|v,i|(i+1..i+v).each{c[_1]+=c[i]}}
 puts"Part2:#{c.sum}"
--- 2023/04.rb
+++ 2023/04.rb
@@ -1,7 +1,7 @@
 input = File.readlines('input.txt')
 
-wins = input.map { |l| l.split(/ \| |: /)[1, 2].map { _1.split.map(&:to_i) }.reduce(&:&).size }
-puts "Part 1: #{wins.map { _1 > 0 ? 2.pow(_1 - 1) : 0 }.sum}"
+wins = input.map { |l| l.split(/ \| |: /)[1, 2].map(&:split).reduce(&:&).size }
+puts "Part 1: #{(wins - [0]).sum { 2**(_1 - 1) }}"
 
 cnt = wins.map { 1 }
 wins.each.with_index { |w, i| (i + 1..i + w).each { cnt[_1] += cnt[i] if cnt[_1] } }
