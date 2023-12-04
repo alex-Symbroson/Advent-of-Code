1b2edf62bc5b92e173a452f03027ec22946db550
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-04 10:37:00 +0100

    23-04 golf update

--- 2023/04 golf.rb
+++ 2023/04 golf.rb
@@ -1,5 +1,5 @@
-w=File.readlines('input.txt').map{|l|l.split(/\||:/)[1,2].
+w=File.readlines('input.txt').map{|l|l.split(/:|\|/)[1,2].
 map{_1.split.map(&:to_i)}.reduce(&:&).size}
-puts"Part1:#{w.map{_1>0?2.pow(_1-1):0}.sum}"
-c=w.map{1};w.each.with_index{|v,i|(i+1..i+v).each{c[_1]+=c[i]if c[_1]}}
+puts"Part1:#{(w-[0]).map{2.pow(_1-1)}.sum}"
+c=w.map{1};w.each.with_index{|v,i|(i+1..i+v).each{c[_1]+=c[i]}}
 puts"Part2:#{c.sum}"
