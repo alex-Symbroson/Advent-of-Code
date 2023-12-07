b31b6cc8d3ea04d1f2339583ad82d9946122ff71
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-07 18:51:07 +0100

    optimized 06, fixed golf versions

--- 2023/04 golf.rb
+++ 2023/04 golf.rb
@@ -1,8 +1,4 @@
-w = $<.map do
-    _, b, c = _1.split(/:|\|/)
-    (b.split & c.split).size
-end
-puts "Part1:#{(w - [0]).sum { 2**(_1 - 1) }}"
-c = w.map { 1 }
-w.each.with_index { |v, i| (i + 1..i + v).each { c[_1] += c[i] } }
-puts "Part2:#{c.sum}"
+w=$<.map{_,b,c=_1.split(/:|\|/);(b.split&c.split).size}
+puts"Part1:#{(w-[0]).sum{2**(_1-1)}}"
+c=w.map{1};w.each.with_index{|v,i|(i+1..i+v).each{c[_1]+=c[i]}}
+puts"Part2:#{c.sum}"
--- 2023/07.rb
+++ 2023/07.rb
@@ -1,8 +1,7 @@
 digx = -> { '0123456789TJQKA'.index(_1).to_s(16) }
-strength = -> { '11 21 22 31 32 41 5'.index(_1) / 3 }
 sorter = ->(a, b) { a[0].to_s + a[1] <=> b[0].to_s + b[1] }
 
-order1 = -> { strength.(_1.chars.tally.values.sort.reverse.join[0, 2]) }
+order1 = ->(c) { c.chars.tally.values.sum { _1**2 }.chr }
 order2 = lambda { |c|
     perms = c.chars.repeated_combination(c.count('0'))
     perms.map { |p| order1.(p.reduce(c) { _1.sub('0', _2) }) }.max
