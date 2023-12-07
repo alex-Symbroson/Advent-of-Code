c9361f72ce8018025f526c413b8ee5b87ca86657
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-07 12:44:05 +0100

    optimized 23-07

--- 2023/07.rb
+++ 2023/07.rb
@@ -1,21 +1,16 @@
 digx = -> { '0123456789TJQKA'.index(_1).to_s(16) }
-sorter = ->((a, _, oa), (b, _, ob)) { oa == ob ? a <=> b : oa <=> ob }
-winnings = -> { _1.sort(&sorter).map.with_index { |(_, b), i| b * (i + 1) }.sum }
+strength = -> { '11 21 22 31 32 41 5'.index(_1) / 3 }
+sorter = ->(a, b) { a[-1].to_s + a[0] <=> b[-1].to_s + b[0] }
+winnings = ->(c) { c.sort(&sorter).map.with_index.sum { _1[1] * (_2 + 1) } }
 
-order1 = lambda { |c|
-    t = c.chars.tally.values.sort.reverse
-    '11 21 22 31 32 41 5'.split.index(t[0, 2].join)
-}
-
+order1 = -> { strength.(_1.chars.tally.values.sort.reverse.join[0, 2]) }
 order2 = lambda { |c|
-    perms = '23456789acde'.chars.repeated_combination(c.count('0'))
+    perms = c.chars.repeated_combination(c.count('0'))
     perms.map { |p| order1.(p.reduce(c) { _1.sub('0', _2) }) }.max
 }
 
-cards = $<.map { [_1.split[0].chars.map(&digx).join, _1.split[1].to_i] }
-cards1 = cards.map { _1 << order1.(_1[0]) }
-puts "Part 1: #{winnings.(cards1)}"
+cards = $<.map { [_1.split[0].gsub(/./, &digx), _1.split[1].to_i] }
+puts "Part 1: #{winnings.(cards.map { _1 << order1.(_1[0]) })}"
 
-cards.map! { |(a, b)| [a.gsub('b', '0'), b] }
-cards2 = cards.map { _1 << order2.(_1[0]) }
-puts "Part 2: #{winnings.(cards2)}"
+cards.map { _1[0].gsub!('b', '0') }
+puts "Part 2: #{winnings.(cards.map { _1 << order2.(_1[0]) })}"
