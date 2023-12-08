5a3fcb0e91fc23682429f6d1928e163d67cb2a75
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-08 08:53:09 +0100

    optimized 23-07

--- 2023/07 golf.rb
+++ 2023/07 golf.rb
@@ -1,5 +1,11 @@
-m=->(c,a=c){c.chars.tally.values.sum{_1**2}.chr+a}
-n=->(c){c.chars.repeated_combination(c.count('0')).map{|p|m.(p.reduce(c){_1.sub('0',_2)},c)}.max}
-d,e=$<.map{|c|[c.split[0].gsub(/./){'0123456789TJQKA'.index(_1).to_s(16)},c.split[1].to_i]}.transpose
-w=->(c){c.zip(d,e).sort.map.with_index.sum{_1[2]*(_2+1)}}
-p w.(d.map(&m));d.map{_1.gsub!('b','0')};p w.(d.map(&n))
+m=->(c,a=c){c.chars.tally.sum{_1[1]**2}.chr+a};
+d,e=$<.map{_1.tr('TKA','BRS').split}.transpose;
+[d.map(&m),d.map{|c|c.tr!('J',o='0');c.chars.repeated_combination(c.count(o))
+.map{|p|m[p.reduce(c){_1.sub(o,_2)},c]}.max}]
+.map{|c|p c.zip(e).sort.map.with_index.sum{_1[1].to_i*(_2+1)}}
+
+# 4x70
+# m=->(c,a=c){c.chars.tally.sum{_1[1]**2}.chr+a};d,e=$<.map{_1.tr('TKA',
+# 'BRS').split}.transpose;[d.map(&m),d.map{|c|c.tr!('J',o='0');c.chars.
+# repeated_combination(c.count(o)).map{|p|m[p.reduce(c){_1.sub(o,_2)},c
+# ]}.max}].map{|c|p c.zip(e).sort.map.with_index.sum{_1[1].to_i*(_2+1)}}
--- 2023/07.rb
+++ 2023/07.rb
@@ -1,14 +1,12 @@
-digx = -> { '0123456789TJQKA'.index(_1).to_s(16) }
-
 order1 = ->(c, a = c) { c.chars.tally.values.sum { _1**2 }.chr + a }
 order2 = lambda { |c|
     perms = c.chars.repeated_combination(c.count('0'))
     perms.map { |p| order1.(p.reduce(c) { _1.sub('0', _2) }, '') }.max + c
 }
 
-cards, bids = $<.map { [_1.split[0].gsub(/./, &digx), _1.split[1].to_i] }.transpose
-winnings = ->(c) { c.zip(bids).sort.map.with_index.sum { _1[1] * (_2 + 1) } }
+cards, bids = $<.map { _1.tr('TKA', 'BRS').split }.transpose
+winnings = ->(c) { c.zip(bids).sort.map.with_index.sum { _1[1].to_i * (_2 + 1) } }
 puts "Part 1: #{winnings.(cards.map(&order1))}"
 
-cards.map { _1.gsub!('b', '0') }
+cards.map { _1.gsub!('J', '0') }
 puts "Part 2: #{winnings.(cards.map(&order2))}"
