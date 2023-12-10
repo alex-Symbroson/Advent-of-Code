72eae16b77e0bd1e2b473bc02a622e77d7521852
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-10 22:14:48 +0100

    optimized 23-10-1

--- 2023/10.rb
+++ 2023/10.rb
@@ -5,26 +5,29 @@ p = [sp % map[0].size, sp / map[0].size]
 dirs = 'J-7 L|J F-L 7|F'.split # RDLU 0123 i+1
 # next: URD RDL DLU LUR          URDL 3012 i/4+i%4
 
-#   0 1 2 3
-# x 1 0-1 0
-# y 0 1 0-1
-
-def pm(ax, ay, _, bx, by, _)
-    m = $map.join.lines
-    m[ay][ax] = '#'
-    m[by][bx] = '#'
-    puts m.join + "\n"
+def pm(ax, ay, ad)
+    m = $map
+    m[ay][ax] = '>v<^'[ad]
+    # puts $map.join.tr('FJL7|-', ' ')
+    # sleep(0.06)
 end
 
-step = ->(x, y, d) { [x += (~d + 2) * (~d % 2), y += (2 - d) * (d % 2), (d + 3 + dirs[d].index(map[y][x])) % 4] }
+step = lambda { |x, y, d|
+    [
+        x += (~d + 2) * (~d % 2),
+        y += (2 - d) * (d % 2),
+        (d + 3 + dirs[d].index(map[y][x])) % 4
+    ] rescue nil
+}
 
-part1 = (0..3).filter_map { step.(*p, _1) rescue nil }.then do |a, b|
+dist = (0..3).filter_map { step.(*p, _1) }.then do |a, _b|
     (2..).find do
-        a = step.(*a)
-        b = step.(*b)
-        a[..1] == b[..1]
+        pm(*a)
+        !(a = step.(*a))
     end
 end
-puts "Part 1: #{part1}"
+
+# puts $map.join.tr('FJL7|-', '#')
+puts "Part 1: #{dist / 2}"
 
 # puts "Part 2: #{sensors.map(&:reverse!).map(&diffs).sum}"
