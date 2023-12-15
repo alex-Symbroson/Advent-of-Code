de39ab494e9159aa633ba1a15392789bd0c31b4d
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-15 11:08:43 +0100

    optimized 23-15

--- 2023/15.rb
+++ 2023/15.rb
@@ -1,20 +1,13 @@
 input = $<.read.tr("\r\n", '').split(',')
 hash = ->(l) { l.bytes.reduce(0) { (_1 + _2) * 17 % 256 } }
-puts "Part 1: #{input.sum(&hash)}"
 
-boxes = Hash.new { _1[_2] = [] }
+boxes = {}
 input.map do |l|
     lbl, op = l.split(/[=-]/)
-    box = boxes[1 + hash[lbl]]
-    if op
-        if i = box.index { _1[0] == lbl }
-            box[i][1] = op.to_i
-        else
-            box.push([lbl, op.to_i])
-        end
-    else
-        box.delete_if { _1[0] == lbl }
-    end
+    box = boxes[1 + hash[lbl]] ||= []
+    i = box.index { _1[0] == lbl } || box.size
+    op ? box[i] = [lbl, op.to_i] : box.delete_at(i)
 end
 
+puts "Part 1: #{input.sum(&hash)}"
 puts "Part 2: #{boxes.sum { |(i, b)| i * b.map.with_index.sum { |l, j| -~j * l[1] } }}"
