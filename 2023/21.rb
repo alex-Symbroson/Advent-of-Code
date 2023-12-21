55b6104a3edb953eb8b0b791f24334aeab538b4f
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-21 12:15:46 +0100

    23-21

--- 2023/21.rb
+++ 2023/21.rb
@@ -1,17 +1,18 @@
 map = $<.map { _1.tr("\r\n", '').chars }
 size = map[0].size + 1i * map.size
-one = 1 + 1i
+n, w = 26_501_365, map.size
+$stdout.sync = true
 
 modC = ->(v, m) { v.real % m.real + 1i * (v.imag % m.imag) }
 step = ->(p, d) { p + (1 - d) * (~d % 2) + 1i * (2 - d) * (d % 2) }
 m = ->(p, v = nil) { map[p.imag][p.real] = v || map[p.imag][p.real] }
 
 vis = Set.new
-vis << (size - one) / 2
+vis << (size - 1 - 1i) / 2
+counts = [0]
+r = n % w;
 
-n = 0
-64.times do
-    n += 1
+(r + 2 * w).times do |i|
     nvis = Set.new
     vis.map do |p|
         4.times do
@@ -20,6 +21,16 @@ n = 0
         end
     end
     vis = nvis
+    print "\r#{100 * i / (r + 2 * w)}%"
+    counts << vis.size
 end
 
-puts "Part 1: #{vis.size}"
+puts
+puts "Part 1: #{counts[64]}"
+
+y1, y2, y3 = counts[r], counts[r + w], counts[r + 2 * w]
+a = (y1 - 2 * y2 + y3) / 2
+b = (-3 * y1 + 4 * y2 - y3) / 2
+x = n / w
+
+puts "Part 2: #{a * x**2 + b * x + y1}"
