1c054a08c885835769a4c667ea9fca3470809c66
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-22 21:47:41 +0100

    22-17-2 wip

--- 2023/22.rb
+++ 2023/22.rb
@@ -15,7 +15,6 @@ supp = []
 
 bricks.map.with_index do |a, _i|
     below = bricks.map.with_index.filter { |b, _| b[1][2] < a[0][2] && its.(a, b) }
-    # p below
     loop do
         dropped = below.filter_map { |b, j| j if a[0][2] - 1 <= b[1][2] }
         break supp << dropped if a[0][2] <= 1 || !dropped.empty?
@@ -28,3 +27,35 @@ end
 hold = supp.map { [] }
 supp.map.with_index { |l, i| l.map { hold[_1] << i } }
 puts "Part 1: #{supp.size.times.count { |i| hold[i].none? { (supp[_1] - [i]).empty? } }}"
+
+# Part 2 WIP
+
+# p(supp.map { |l| l.map { 'ABCDEFGH'[_1] }.join })
+# p(hold.map { |l| l.map { 'ABCDEFGH'[_1] }.join })
+
+safe = (0...supp.size).filter { |i| !(supp[i] - [i]).empty? }
+puts "Part 1: #{safe.size}"
+nsafe = 0
+
+sum = (0...supp.size).to_a.sum do |i|
+    next (nsafe += 1; p i if i < 20; 0) if hold[i].map { p([i, supp[_1]]) if i < 20; supp[_1].size > 1 }
+
+    vis = Set.new
+    queue = hold[i]
+    while j = queue.pop
+        next if hold[j].none? { (supp[_1] - [j]).empty? }
+
+        vis << j
+        queue += hold[j].filter { !(vis === _1) }
+    end
+    # puts "#{i} #{vis.size}" if i < 10
+    # p vis.to_a unless vis.empty?
+    vis.size
+end
+puts "Part 1: #{nsafe}"
+
+if sum < 87_123 && sum > 1122 && sum != 40_803 && sum != 39_968
+    puts "Part 2: #{sum}"
+else
+    p sum
+end
