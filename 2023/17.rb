1837b518dc3284878e20347e06a026e9cf9ebda5
alex-Symbroson <alex.symbroson@gmail.com>
2023-12-22 03:51:49 +0100

    23-17-1

--- 2023/17.rb
+++ 2023/17.rb
@@ -4,6 +4,7 @@ $stdout.sync = true
 map = $<.map { _1.tr("\n", '').chars.map(&:to_i) }
 $dir2 = ->(((a, b), (c, d))) { [c - a, d - b] }
 $m = ->((x, y)) { map[y][x] }
+w, h = map[0].size, map.size
 
 $printmap = lambda { |path|
     map2 = map.map(&:join)
@@ -11,32 +12,30 @@ $printmap = lambda { |path|
     puts map2.join("\n") + "\n\n"
 }
 
+$npath = 3
+# $npath = 10
+
 next_node_fn = lambda do |node, _cost, path|
     x, y = node
+    lx, ly = path[-2] || [-1, 0]
     neighbors = []
-    same1 = path[-4..].each_cons(2).map(&$dir2).uniq if path && path.size > 3
+    same1 = path[~$npath..].each_cons(2).map(&$dir2).uniq if path && path.size > $npath
     same = same1 && same1.size == 1 ? same1[0] : nil
 
-    neighbors << [x + 1, y] if same != [1, 0] && x + 1 < map[0].length
-    neighbors << [x - 1, y] if same != [-1, 0] && x - 1 >= 0
-    neighbors << [x, y + 1] if same != [0, 1] && y + 1 < map.length
-    neighbors << [x, y - 1] if same != [0, -1] && y - 1 >= 0
-
-    # p([node, same1, path[-4..], neighbors])
-
-    # puts cost
-    # $printmap[path + [node]]
-    # sleep 0.3
+    neighbors << [x + 1, y] if same != [1, 0] && lx - x != 1 && x + 1 < w
+    neighbors << [x - 1, y] if same != [-1, 0] && lx - x != -1 && x - 1 >= 0
+    neighbors << [x, y + 1] if same != [0, 1] && ly - y != 1 && y + 1 < h
+    neighbors << [x, y - 1] if same != [0, -1] && ly - y != -1 && y - 1 >= 0
 
     neighbors
 end
 
 Node = Struct.new(:pos, :dir, :prev, :cost)
-def makePath(node)
-    path = [node.pos]
-    while node.prev
+def makePath(node, n = 1e9, key = :pos)
+    path = [node[key]]
+    while node.prev && (n -= 1) > 0
         node = node.prev
-        path << node.pos
+        path << node[key]
     end
     path.reverse!
 end
@@ -47,10 +46,8 @@ def find_path(src_node, goal_test, cost_fn, next_node_fn)
     queue = PQueue.new { |(c1, _n1), (c2, _n2)| c1 < c2 }
 
     makekey = lambda { |node1|
-        path2 = makePath(node1)
-        path1 = path2.reverse[0..3].reverse if path2.size > 1
-        same1 = path1.each_cons(2).map(&$dir2).uniq if path1
-        [node1.pos, node1.dir, same1 ? same1.size : 0].hash
+        path2 = makePath(node1, $npath, :dir)
+        [node1.pos, node1.dir, *path2].hash # , same1 ? same1.size : 0].hash
     }
 
     queue << [0, Node.new(src_node, [0, 0], nil, 0)]
@@ -74,7 +71,7 @@ def find_path(src_node, goal_test, cost_fn, next_node_fn)
         # sleep(0.05)
 
         visited << makekey[node]
-        for next_pos in next_node_fn[node.pos, node.cost, makePath(node)]
+        for next_pos in next_node_fn[node.pos, node.cost, makePath(node, $npath + 1)]
             dir = $dir2[[node.pos, next_pos]]
             next_node = Node.new(next_pos, dir, node, 0)
 
@@ -83,10 +80,10 @@ def find_path(src_node, goal_test, cost_fn, next_node_fn)
             next_node.cost = next_cost
 
             # puts "test #{next_pos} #{cost} -> #{next_cost} < #{prev_cost}: #{!prev_cost || next_cost < prev_cost}"
-            # if !prev_cost || next_cost < prev_cost + 80
-            costs[makekey[next_node]] = next_cost
-            queue << [next_cost, next_node]
-            # end
+            if !prev_cost || next_cost < prev_cost
+                costs[makekey[next_node]] = next_cost
+                queue << [next_cost, next_node]
+            end
         end
         # puts "\n\n"
     end
