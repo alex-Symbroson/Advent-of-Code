bb38eca0ce23f4db6de25c6f6d8a0c5e3eec0612
alex-Symbroson <alex.symbroson@gmail.com>
2024-12-01 21:25:39 +0100

    24-01 update

--- 2024/01.rb
+++ 2024/01.rb
@@ -1,12 +1,6 @@
-digits = $<.map { |line| line.scan(/\d+/).map { |n| n.to_i } }
-sorted = digits.transpose.map { |l| l.sort }
-comp = sorted.transpose.map { |e| (e[0] - e[1]).abs }
-
+digits = $<.map { |line| line.scan(/\d+/).map(&:to_i) }
+comp = digits.transpose.map(&:sort).transpose.map { |e| (e[0] - e[1]).abs }
 print('Part 1: ', comp.sum, "\n")
 
-counts = digits.transpose.map { |l| l.tally }
-c1 = counts[0]
-c2 = counts[1]
-c2.default = 0
-
-print('Part 2: ', c1.map {|k,v| k * v * (c2[k]) }.sum)
+c1, c2 = digits.transpose.map(&:tally)
+print('Part 2: ', c1.map { |k, v| k * v * c2.fetch(k, 0) }.sum)
