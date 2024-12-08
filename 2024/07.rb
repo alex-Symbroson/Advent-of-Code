dc2d4c344557eff37dc09c6a3cd6d6b108d0d675
alex-Symbroson <alex.symbroson@gmail.com>
2024-12-08 15:34:04 +0100

    24-07 update

--- 2024/07.rb
+++ 2024/07.rb
@@ -1,23 +1,16 @@
 lines = $<.map{_1.scan(/\d+/).map(&:to_i)}
 
-part1 = lines.sum { |(s,*l)| (2**(l.size-1)).times.any? { |m|
-    s == l.each.with_index.reduce { |(a,ai), (b,bi)|
-        (m >> (bi-1)) % 2 == 0 ? a+b : a*b
-        # [a+b, a*b][(m >> (bi-1)) % 2]
+calc = ->(r) {
+    lines.sum { |(s, *l)|
+        (r ** (l.size-1)).times.any? { |m|
+            s == l.each.with_index.reduce { |(a,ai), (b,bi)|
+                f = (m / r ** (bi - 1)) % r
+                d = b < 10 ? 10 : (b < 100 ? 100 : 1000)
+                f==0 ? a+b : (f==1 ? a*b : a*d+b)
+            }
+        } ? s : 0
     }
-} ? s : 0}
-
-print('Part 1: ', part1, "\n")
-
-part2 = lines.sum { |(s,*l)|
-    (3**(l.size-1)).times.any? { |m|
-        s == l.each.with_index.reduce { |(a,ai), (b,bi)|
-            f = (m / 3**(bi - 1)) % 3
-            d = b < 10 ? 10 : (b < 100 ? 100 : 1000)
-            f==0 ? a+b : (f==1 ? a*b : a*d+b)
-            #[a+b, a*b, a*d+b][f]
-        }
-    } ? s : 0
 }
 
-print('Part 2: ', part2, "\n") # 90s
+print('Part 1: ', calc[2], "\n") #  1s
+print('Part 2: ', calc[3], "\n") # 90s
