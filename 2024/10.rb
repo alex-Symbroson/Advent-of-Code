adb76bc420320d08e0bb884466d0a3024b857be8
alex-Symbroson <alex.symbroson@gmail.com>
2024-12-10 17:28:07 +0100

    24-10 update

--- 2024/10.rb
+++ 2024/10.rb
@@ -1,24 +1,16 @@
 map = $<.map{_1.strip.chars.map { |c| c=='.' ? nil : c.to_i}}
 w,h = map[0].size, map.size
 dirs = [[-1,0], [1,0], [0,-1], [0,1]]
-m={}
 
 walk = ->(x, y, v) {
     v == 9 ? x+y*1i : dirs.flat_map { |(dx,dy)|
         y+dy<0 || x+dx<0 || y+dy>=h || map[y+dy][x+dx] != v+1 ? nil : walk[x+dx,y+dy,v+1]
-    }
+    }.compact
 }
 
-part1 = h.times.sum { |y| w.times.sum { |x| map[y][x] == 0 ? walk[x,y,0].compact.uniq.count : 0}}
-
-print('Part 1: ', part1, "\n")
-
-walk2 = ->(x, y, v) {
-    v == 9 ? 1 : dirs.sum { |(dx,dy)|
-        y+dy<0 || x+dx<0 || y+dy>=h || map[y+dy][x+dx] != v+1 ? 0 : walk2[x+dx,y+dy,v+1]
-    }
+walks = (h*w).times.sum { |x|
+    map[x/w][x%w] > 0 ? 0 : (r = walk[x%w,x/w,0]; r.uniq.count+r.count*1i)
 }
 
-part2 = h.times.sum { |y| w.times.sum { |x| map[y][x] == 0 ? walk2[x,y,0] : 0}}
-
-print('Part 2: ', part2, "\n")
+print('Part 1: ', walks.real, "\n")
+print('Part 2: ', walks.imag, "\n")
