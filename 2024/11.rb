a6d6aba6ca35e0645363994825aa39c8b65fe2e2
alex-Symbroson <alex.symbroson@gmail.com>
2024-12-12 10:14:47 +0100

    24-11

---
+++ 2024/11.rb
@@ -0,0 +1,21 @@
+map = $<.read.split(" ").map(&:to_i).tally
+
+b = ->(v) {
+    if v == 0 then [1]
+    elsif v == 1 then [2024]
+    elsif (s = v.to_s.size).odd? then [v*2024]
+    else
+        d = 10**(s/2)
+        [v/d, v%d]
+    end
+}
+
+s = ->(i) {
+    step = map.map { |k,v| b[k].tally.transform_values{v*_1}}
+    map = {}.merge(*step) {_2 + _3}
+}
+
+25.times(&s)
+print('Part 1: ', map.values.sum, "\n")
+50.times(&s)
+print('Part 2: ', map.values.sum, "\n")
