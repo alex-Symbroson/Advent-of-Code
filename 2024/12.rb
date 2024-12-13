7233890e0eeaca2a6fae987a077709826e2f024e
alex-Symbroson <alex.symbroson@gmail.com>
2024-12-13 13:56:08 +0100

    24-12 update

--- 2024/12.rb
+++ 2024/12.rb
@@ -8,10 +8,7 @@ fill = ->(x,y,s,r) {
     next 1 if y<0 || x<0 || y>=h || x>=w
     next 0 if map[y][x] == s.downcase
     next 1 if map[y][x] == map[y][x].downcase
-    if map[y][x] != s then
-        l << [x,y,map[y][x]]
-        next 1
-    end
+    next (l << [x,y,map[y][x]]; 1) if map[y][x] != s
     map[y][x] = r
     1i + dirs.each.with_index.sum { |(dx,dy), d|
         t = fill[x+dx, y+dy, s,r]
@@ -20,18 +17,18 @@ fill = ->(x,y,s,r) {
     }
 }
 
-def countMergable(edges)
-    dist = ->((a,b), (c,d)) { (a-c).abs + (b-d).abs }
-    edges.sum { |e| edges.count { |f|
-        e[2] == f[2] && dist[e,f] == 1
-    }} / 2
-end
+dist = ->((a,b), (c,d)) { (a-c).abs + (b-d).abs }
+countMergable = ->(edges) {
+    edges.repeated_permutation(2).count {
+        |e,f| e[2] == f[2] && dist[e,f] == 1
+    } / 2
+}
 
 m = []
 while e = l.pop do
     edges.clear
     r = fill[*e, e[2].downcase]
-    m << [e[2], r.imag, r.real, countMergable(edges)]
+    m << [e[2], r.imag, r.real, countMergable[edges]]
 end
 
 print('Part 1: ', m.sum{_2*_3}, "\n")
