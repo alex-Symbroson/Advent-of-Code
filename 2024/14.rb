8ca68832c1b19ec579f883c2ffb9a94e9c8bfceb
alex-Symbroson <alex.symbroson@gmail.com>
2024-12-19 11:43:43 +0100

    24-14 count in array

--- 2024/14.rb
+++ 2024/14.rb
@@ -1,7 +1,7 @@
 list = $<.map{_1.scan(/[-\d]+/).map(&:to_i)}
 w, h = 101, 103
 wq, hq = w/2+1, h/2+1
-mx, my = Hash.new(0),Hash.new(0)
+mx, my = Array.new(w,0),Array.new(h,0)
 
 step = ->(v, d=1) {
     v[0] = (v[0] + d*v[2]) % w
@@ -25,10 +25,10 @@ puts "Part 1: #{sums.values.reduce(&:*)}"
 
 l = []
 part2 = (101..).each {
-    mx.clear
-    my.clear
+    mx.fill(0)
+    my.fill(0)
     list.each(&step)
-    l << _1 if mx.values.max > 30 || my.values.max > 30
+    l << _1 if mx.max > 30 || my.max > 30
     break if l.size == 2
 }
 
